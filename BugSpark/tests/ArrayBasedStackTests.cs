6db32299abf9b31347f7f55788ab8911868df446
Khaled Nassar <khaled.nassar99@eng-st.cu.edu.eg>
2020-12-30 20:55:50 +0200

    restructuring

---
+++ BugSpark/tests/ArrayBasedStackTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BugSpark
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void StackInit()
+        {
+            var stack = new ArrayBasedStack<int> ();
+            stack.Push(3);
+            Assert.AreEqual(stack.Peek(), 3);
+        }
+    }
+}
