427b41850c357f41e7b711c531cfe053c0ee1403
Ayman Azzam <aymanazzam63@gmail.com>
2020-12-30 19:18:26 +0200

    Upload Algorithms SUT

---
+++ BugSpark/tests/AlgorithmsTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BugSpark
+{
+    [TestFixture]
+    public class AlgorithmsTests
+    {
+        [Test]
+        public void Test1()
+        {
+           
+        }
+    }
+}
