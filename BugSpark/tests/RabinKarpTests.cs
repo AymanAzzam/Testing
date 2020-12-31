335cf3b7a5b225224966f65857b5c74a4f6664cc
Ayman <ayman.elakwah@gmail.com>
2020-12-31 03:37:35 +0200

    RabinKarp branch coverage

---
+++ BugSpark/tests/RabinKarpTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BugSpark
+{
+    [TestFixture]
+    public class RabinKarpTests
+    {
+        [Test]
+        public void FindAllOccurrences_Branch_Test_1()
+        {
+            List<int> expected_output = new List<int>() {0};
+            Assert.AreEqual(RabinKarp.FindAllOccurrences("heÌ", "h"), expected_output);
+        }
+    }
+}
