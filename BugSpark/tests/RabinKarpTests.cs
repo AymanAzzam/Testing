70a202bf7e58e1be59caa3796e5aa396a5f60b66
Ayman <ayman.elakwah@gmail.com>
2021-01-05 23:09:58 +0200

    rename ISP to IDM

--- BugSpark/tests/RabinKarpTests.cs
+++ BugSpark/tests/RabinKarpTests.cs
@@ -42,91 +42,91 @@ namespace BugSpark
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_1()
+        public void FindAllOccurrences_IDM_Test_1()
         {
             List<int> expected_output = new List<int>() {8};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("This is great", "great"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_2()
+        public void FindAllOccurrences_IDM_Test_2()
         {
             List<int> expected_output = new List<int>() {};
             Assert.AreEqual(RabinKarp.FindAllOccurrences(null, "great"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_3()
+        public void FindAllOccurrences_IDM_Test_3()
         {
             List<int> expected_output = new List<int>() {};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("This is great", null), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_4()
+        public void FindAllOccurrences_IDM_Test_4()
         {
             List<int> expected_output = new List<int>() {};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("", "hello"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_5()
+        public void FindAllOccurrences_IDM_Test_5()
         {
             List<int> expected_output = new List<int>() {0};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("a", "a"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_6()
+        public void FindAllOccurrences_IDM_Test_6()
         {
             List<int> expected_output = new List<int>() {1};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("ab", "b"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_7()
+        public void FindAllOccurrences_IDM_Test_7()
         {
             List<int> expected_output = new List<int>() {};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello", ""), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_8()
+        public void FindAllOccurrences_IDM_Test_8()
         {
             List<int> expected_output = new List<int>() {4};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello", "o"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_9()
+        public void FindAllOccurrences_IDM_Test_9()
         {
             List<int> expected_output = new List<int>() {3};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello", "lo"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_10()
+        public void FindAllOccurrences_IDM_Test_10()
         {
             List<int> expected_output = new List<int>() {};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello", "how"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_11()
+        public void FindAllOccurrences_IDM_Test_11()
         {
             List<int> expected_output = new List<int>() {0, 6};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello hello", "hello"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_12()
+        public void FindAllOccurrences_IDM_Test_12()
         {
             List<int> expected_output = new List<int>() {};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello", "how"), expected_output);
         }
         
         [Test]
-        public void FindAllOccurrences_ISP_Test_13()
+        public void FindAllOccurrences_IDM_Test_13()
         {
             List<int> expected_output = new List<int>() {0};
             Assert.AreEqual(RabinKarp.FindAllOccurrences("hello", "hello"), expected_output);
