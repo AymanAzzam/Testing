c449515a72420ecc263686bfd3880b4562043804
Khaled Nassar <khaled.nassar99@eng-st.cu.edu.eg>
2021-01-05 16:27:34 +0200

    finished Edge coverage for stack

--- BugSpark/tests/ArrayBasedStackTests.cs
+++ BugSpark/tests/ArrayBasedStackTests.cs
@@ -21,6 +21,17 @@ namespace BugSpark
         }
         
         [Test]
+        public void BCC_5()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (4);
+            Assert.AreEqual(0, stack.Count);
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                stack.Pop();
+            });
+        }
+        
+        [Test]
         public void BCC_6()
         {
             ArrayBasedStack<int> stack = new ArrayBasedStack<int> (4);
@@ -39,6 +50,24 @@ namespace BugSpark
             stack.Push(1);
             Assert.IsTrue(stack.Contains(10));
             Assert.AreNotEqual(stack.Count, stack.Capacity);
+        }
+        
+        [Test]
+        public void BCC_8()
+        {
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                ArrayBasedStack<int> stack = new ArrayBasedStack<int>(-1);
+            });
+        }
+        
+        [Test]
+        public void BCC_9()
+        {
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                ArrayBasedStack<int> stack = new ArrayBasedStack<int>(0);
+            });
         }
         
         [Test]
@@ -102,6 +131,117 @@ namespace BugSpark
             Assert.AreEqual(stack.Count, stack.Capacity);
             stack.Push(5);
             Assert.AreNotEqual(stack.Count, stack.Capacity);
+        }
+        
+        // Edge Coverage
+        [Test]
+        public void EC_Constructor_1()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (4);
+        }
+        
+        [Test]
+        public void EC_Constructor_2()
+        {
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                ArrayBasedStack<int> stack = new ArrayBasedStack<int>(-1);
+            });
+        }
+        
+        [Test]
+        public void EC_Capacity_1()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (1);
+            Assert.AreEqual(1, stack.Capacity);
+        }
+        
+        [Test]
+        public void EC_Capacity_2()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (1);
+            stack.Capacity = 10;
+            Assert.AreEqual(10, stack.Capacity);
+        }
+        
+        [Test]
+        public void EC_Clear()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (1);
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Count);
+            stack.Clear();
+            Assert.AreEqual(0, stack.Count);
+        }
+        
+        [Test]
+        public void EC_Peek_1()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (4);
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Peek());
+        }
+        
+        [Test]
+        public void EC_Peek_2()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int>(1);
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                stack.Peek();
+            });
+        }
+        
+        [Test]
+        public void EC_Pop_1()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (4);
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Count);
+            stack.Pop();
+            Assert.AreEqual(1, stack.Count);
+        }
+        
+        [Test]
+        public void EC_Pop_2()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int>(1);
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                stack.Pop();
+            });
+        }
+        
+        [Test]
+        public void EC_Push_1()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (4);
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Count);
+        }
+        
+        [Test]
+        public void EC_Push_2()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (2);
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(stack.Capacity, stack.Count);
+            stack.Push(3);
+            // Capacity is doubled to 8
+            Assert.AreNotEqual(stack.Capacity, stack.Count);
+        }
+        
+        [Test]
+        public void EC_Contains()
+        {
+            ArrayBasedStack<int> stack = new ArrayBasedStack<int> (2);
+            stack.Push(1);
+            stack.Push(2);
+            Assert.IsTrue(stack.Contains(1));
+            Assert.IsFalse(stack.Contains(5));
         }
     }
 }
